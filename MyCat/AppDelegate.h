992f29123d727c0a911128bfc023a10476690d87
Damoness-Mini <qjwdcool@qq.com>
2017-12-10 15:12:32 +0800

    Initial Commit

---
+++ MyCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCat
+//
+//  Created by Damoness on 2017/12/10.
+//  Copyright © 2017年 damoness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCat
+//
+//  Created by Damoness on 2017/12/10.
+//  Copyright © 2017年 damoness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
